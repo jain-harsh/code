e7cf968da7b8c6c33725f019adbdf3a0c3c1c906
harshit <jainharshit218@gmail.com>
2020-03-04 17:30:24 +0530

    commit

---
+++ my-project/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,53 @@
+package com.mycompany.app;
+import org.dcm4che2.io.*;
+import org.dcm4che2.util.TagUtils;
+import org.dcm4che2.data.DicomElement;
+import org.dcm4che2.data.DicomObject;
+
+import java.util.*;
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+         DicomObject object = null;  
+           try {
+              DicomInputStream dis = new DicomInputStream(new File("E:\\000000.dcm"));
+              object = dis.readDicomObject();
+              dis.close();
+           } catch (Exception e) {
+              System.out.println(e);
+              System.exit(0);
+           }
+          App list = new App();
+           list.listHeader(object);
+    }
+    public void listHeader(DicomObject object) {
+           Iterator iter = object.datasetIterator();
+           while(iter.hasNext()) {
+              DicomElement element = (DicomElement) iter.next();
+              int tag = element.tag();
+              try {
+                 String tagName = object.nameOf(tag);
+                 String tagAddr = TagUtils.toString(tag);
+                 String tagVR = object.vrOf(tag).toString();
+                 if (tagVR.equals("SQ")) {
+                    if (element.hasItems()) {
+                       System.out.println(tagAddr +" ["+  tagVR +"] "+ tagName);
+                       listHeader(element.getDicomObject());
+                       continue;
+                    }
+                 }    
+                 String tagValue = object.getString(tag);    
+                 System.out.println(tagAddr +" ["+ tagVR +"] "+ tagName +" ["+ tagValue+"]");
+              } catch (Exception e) {
+                 e.printStackTrace();
+              }
+           }  
+        }
+}
